4a9abe195a5db487296cd063c6833e5a05cf4619
Alekss Verskovs <64939153+JiKiDJi@users.noreply.github.com>
2023-04-12 19:07:28 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -31,10 +31,8 @@ def get_occurrences(pattern, text):
                 return hmap[j][1]
         return(-1)
         
-    n = len(pattern)
+    n = len(pattern)-1
     l = len(text)
-    print(pattern)
-    print(text)
     
     num = 0
     for i in range(n):
@@ -60,7 +58,7 @@ def get_occurrences(pattern, text):
             hmap.append([text[i],len(hmap)+1])
             num+=len(hmap)
         nums.append(num)
-    print(nums)
+
     num = 0
     for i in range(n):
         num *= 10
